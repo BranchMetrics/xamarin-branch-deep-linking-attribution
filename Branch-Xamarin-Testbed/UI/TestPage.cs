999856d2ba6fac2d39aca249844d74bf71678922
nsingh-branch <nipun.singh@branch.io>
2022-11-17 14:05:42 -0800

    Revert "Updated testbed"
    This reverts commit 0088b7ea78284b3a979b51132fcb61b4651197ec.

--- Branch-Xamarin-Testbed/UI/TestPage.cs
+++ Branch-Xamarin-Testbed/UI/TestPage.cs
@@ -1,13 +1,14 @@
 ﻿using BranchXamarinSDK;
 
 using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.ComponentModel;
 using Xamarin.Forms;
 
 namespace BranchXamarinTestbed
 {
-	public class TestPage : ContentPage ,
+	public class TestPage : ContentPage,
 	IBranchUrlInterface,
 	IBranchSessionInterface,
 	IBranchIdentityInterface
@@ -369,13 +370,15 @@ namespace BranchXamarinTestbed
 
 			Branch.GetInstance().GetShortURL(this, universalObject, linkProperties);
 
-            BranchUniversalObject buoTwo = new BranchUniversalObject();
-            buoTwo.canonicalIdentifier = "id2";
-            buoTwo.metadata.AddCustomMetadata("testCustom", "testData");
+            BranchUniversalObject buo2 = new BranchUniversalObject();
+            buo2.canonicalIdentifier = "id2";
+            buo2.metadata.quantity = 2;
+            buo2.metadata.AddCustomMetadata("testCustom", "testData");
+            Debug.WriteLine("Branch Universal Object 2: " + buo2);
 
             List<BranchUniversalObject> buos = new List<BranchUniversalObject>();
             buos.Add(universalObject);
-			buos.Add(buoTwo);
+			buos.Add(buo2);
 
             BranchEvent branchEvent = new BranchEvent("MY_CUSTOM_EVENT");
             branchEvent.SetAffiliation("my_affilation");
@@ -390,6 +393,7 @@ namespace BranchXamarinTestbed
             branchEvent.AddContentItems(buos);
 
             Branch.GetInstance().SendEvent(branchEvent);
+            Debug.WriteLine("Branch Event: " + branchEvent);
 		}
 			
 		void SendEmailClicked(object sender, EventArgs e) {
